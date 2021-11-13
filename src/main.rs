945a5332c4d1e9e0960563a86209cb6b3c89ac83
Hammam Firdaus <hammamxalf@gmail.com>
2021-11-13 14:08:52 +0700

    next init master

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod => include file
 mod routers;
 
-// use actix
+// use depedencies
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use log::{info};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Init Server....");
+    dotenv::dotenv().ok();
+    info!("booting up!");
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(home))
