ad8ea28b2b4471252faf9372da72f4a4dae280e2
Hammam Firdaus <hammamxalf@gmail.com>
2021-11-14 05:15:19 +0700

    use orm diesel

---
+++ migrations/2021-11-13-220139_create_users/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+CREATE TABLE users (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+)
---
+++ src/schema.rs
@@ -0,0 +1,8 @@
+table! {
+    users (id) {
+        id -> Nullable<Text>,
+        name -> Text,
+        email -> Text,
+        password -> Text,
+    }
+}
